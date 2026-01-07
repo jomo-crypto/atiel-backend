39eba8a603ab28e11b69af20b848fb4e29b29e5e
jomo-crypto <mussakayange@gmail.com>
2026-01-07 13:20:06 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 100,
   queueLimit: 0
 });
 
